5c43a08ece927e3b715f4cd792cab8877d543b8d
SuZhoue-Joe <suzhou@amazon.com>
2023-06-10 23:07:19 +0800

    feat: add zh csv

--- utils/app/prompt.ts
+++ utils/app/prompt.ts
@@ -1,11 +1,14 @@
 // @ts-ignore
 import en from '@/default_prompts/prompts_en.csv';
+// @ts-ignore
+import zh from '@/default_prompts/prompts_zh.csv';
 
 export type Prompt = {
   title: string;
   prompt: string;
-}
+};
 
 export const allPrompts: Record<string, Prompt[]> = {
   en,
+  zh,
 };
